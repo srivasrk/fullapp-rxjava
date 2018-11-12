17ef9717f4092224fc3776e93b8bac23fa719f28
Rahul Srivastava <rahsriv@amazon.com>
2018-11-12 16:08:06 -0500

    First commit

---
+++ src/main/java/com/rahul/fullapprxjava/FullappRxjavaApplication.java
@@ -0,0 +1,12 @@
+package com.rahul.fullapprxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullappRxjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullappRxjavaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rahul/fullapprxjava/FullappRxjavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rahul.fullapprxjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullappRxjavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
